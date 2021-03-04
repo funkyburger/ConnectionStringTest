5ecaa2f93b264eedd56b1ca9c0a427f38c82efc3
Mikael Lindh <mslindh@gmail.com>
2021-03-04 22:21:55 +0100

    First version.

---
+++ ConnectionStringTest/ConnectionStringTester.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionStringTest
+{
+    public static class ConnectionStringTester
+    {
+        public static TestResponse Test(string connectionString)
+        {
+            //return new TestResponse(true, "asdasdasd");
+            //return new TestResponse(false, "asdasdasd");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return new TestResponse(true, "Connected successfully.");
+                }
+                catch(Exception e)
+                {
+                    return new TestResponse(false, e.Message);
+                }
+            }
+
+            //connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
+            //cnn = new SqlConnection(connetionString);
+            //cnn.Open();
+            //MessageBox.Show("Connection Open  !");
+            //cnn.Close();
+        }
+    }
+}
---
+++ ConnectionStringTest/TestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionStringTest
+{
+    public class TestResponse
+    {
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+
+        public TestResponse(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
