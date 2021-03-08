d41281738006aa1ff6575e5b3c00ed224b64a178
Mikael Lindh <mslindh@gmail.com>
2021-03-08 17:50:28 +0100

    Implemented file service.

--- ConnectionStringTest/Data/FileService.cs
+++ ConnectionStringTest/Data/FileService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,34 @@ namespace ConnectionStringTest.Data
 {
     public class FileService : IFileService
     {
+        private readonly string Filename = $"{Path.GetTempPath()}cst.tmp";
+
         public string LoadApplicationDataFileContent()
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var stream = File.OpenRead(Filename))
+                {
+                    byte[] buffer = new byte[(int)stream.Length];
+                    stream.Read(buffer, 0, (int)stream.Length);
+
+                    return Encoding.ASCII.GetString(buffer);
+                }
+            }
+            catch(FileNotFoundException)
+            {
+                return string.Empty;
+            }
         }
 
         public void SaveApplicationDataFileContent(string content)
         {
-            throw new NotImplementedException();
+            using (var stream = File.OpenWrite(Filename))
+            {
+                stream.Write(Encoding.ASCII.GetBytes(content), 0, content.Length);
+            }
+
+            File.Encrypt(Filename);
         }
     }
 }
