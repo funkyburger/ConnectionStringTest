c9acfb7e00703c5726d540a257b6edeef95469ed
Mikael Lindh <mslindh@gmail.com>
2021-04-05 15:38:59 +0200

    Added password hider.

---
+++ ConnectionStringTest.UnitTests/Utils/PasswordHiderTests.cs
@@ -0,0 +1,46 @@
+﻿using ConnectionStringTest.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Shouldly;
+using ConnectionStringTest.Utils;
+
+namespace ConnectionStringTest.UnitTests.Utils
+{
+    [TestClass]
+    public class PasswordHiderTests
+    {
+        [TestMethod]
+        public void HidesPasswordIfNeeded()
+        {
+            var hider = new PasswordHider();
+
+            hider.Hide("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;User Id=episandbox; Password=p4$$w0rd;")
+                .ShouldBe("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;User Id=episandbox; Password=●●●●●●●●●;");
+            hider.Hide("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;User Id=episandbox; Password=\"p4$$w0rd\";")
+                .ShouldBe("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;User Id=episandbox; Password=\"●●●●●●●●●\";");
+            hider.Hide("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;User Id=episandbox; Password=\"p4$$w0rd\"")
+                .ShouldBe("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;User Id=episandbox; Password=\"●●●●●●●●●\"");
+            hider.Hide("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;User Id=episandbox; Password=p4$$w0rd")
+                .ShouldBe("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;User Id=episandbox; Password=●●●●●●●●●");
+            hider.Hide("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;          Password        =      \"p4$$w0rd\"        ;User Id=episandbox;")
+                .ShouldBe("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;          Password        =      \"●●●●●●●●●\"        ;User Id=episandbox;");
+            hider.Hide("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;          Password        =      p4$$w0rd        ;User Id=episandbox;")
+                .ShouldBe("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=C:\\Temp\\EpiSandbox.mdf;Connection Timeout=60;          Password        =      ●●●●●●●●●        ;User Id=episandbox;");
+        }
+
+        [TestMethod]
+        public void DoesntHidePasswordIfNotNeeded()
+        {
+        }
+
+        [TestMethod]
+        public void LetStringUnchangedIfPassordIsAlreadyHidden()
+        {
+        }
+    }
+}
---
+++ ConnectionStringTest/Utils/PasswordHider.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConnectionStringTest.Utils
+{
+    public class PasswordHider : IPasswordHider
+    {
+        public string Hide(string connectionString)
+        {
+            var boundaries = GetHiddenTextBoudaries(connectionString);
+
+            if(boundaries != null)
+            {
+                return Mask(connectionString, boundaries);
+            }
+
+            return connectionString;
+        }
+
+        private Tuple<int, int> GetHiddenTextBoudaries(string connectionString)
+        {
+            var passwordSegmentRegex = new Regex("(^|;)\\s*Password\\s*=\\s*(?<password>[^\";\\s]+)\\s*($|;)");
+
+            var matches = passwordSegmentRegex.Matches(connectionString);
+
+            foreach(Match match in matches)
+            {
+                foreach (Group group in match.Groups)
+                {
+                    if(group.Name == "password")
+                    {
+                        return new Tuple<int, int>(group.Index, group.Length);
+                    }
+                }
+            }
+
+            var passwordWithQuotesSegmentRegex = new Regex("(^|;)\\s*Password\\s*=\\s*\"(?<password>[^\"]+)\"\\s*($|;)");
+            matches = passwordWithQuotesSegmentRegex.Matches(connectionString);
+
+            foreach (Match match in matches)
+            {
+                foreach (Group group in match.Groups)
+                {
+                    if (group.Name == "password")
+                    {
+                        return new Tuple<int, int>(group.Index, group.Length);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private string Mask(string connectionString, Tuple<int, int> boudaries)
+        {
+            var builder = new StringBuilder(connectionString.Substring(0, boudaries.Item1));
+            for(int i = 0; i <= boudaries.Item2; i++)
+            {
+                builder.Append('\u25CF');
+            }
+
+            builder.Append(connectionString.Substring(boudaries.Item1 + boudaries.Item2));
+            return builder.ToString();
+        }
+    }
+}
